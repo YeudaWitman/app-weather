1ac460580a516755345d71aa3e9fa6d47aede546
yeuda.witman <yeudaww@gmail.com>
2020-08-03 16:21:03 +0300

    Updates

---
+++ precache-manifest.525bc826d097498544654c741dc85f46.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d37967183514af24324799040b7e470c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d044b43a79a800610eb1",
+    "url": "/static/js/2.f9af28e5.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.f9af28e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4eea9b3150a5b7f706c4",
+    "url": "/static/js/main.4f9a08e9.chunk.js"
+  },
+  {
+    "revision": "e45b57012750ba560b9e",
+    "url": "/static/js/runtime-main.098ada1f.js"
+  }
+]);
