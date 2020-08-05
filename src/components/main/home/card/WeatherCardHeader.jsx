a2defac5742904c2c91de571e8155ebaef975240
yeuda.witman <yeudaww@gmail.com>
2020-08-05 11:40:13 +0300

    start dynamic icon indicator

--- src/components/main/home/card/WeatherCardHeader.jsx
+++ src/components/main/home/card/WeatherCardHeader.jsx
@@ -6,6 +6,10 @@ import { CardHeader, Avatar, Icon, Typography } from '@material-ui/core';
 import FavoriteButton from './FavoriteButton.jsx';
 import CityTitle from './CityTitle.jsx';
 
+const icons = [
+  null, "wb_sunny"
+]
+
 const useStyles = makeStyles((theme) => ({
   large: {
     width: theme.spacing(7),
@@ -17,16 +21,16 @@ const useStyles = makeStyles((theme) => ({
 const WeatherCardHeader = ({ data, city }) => {
   const system = 'Metric';
   //const system = 'Imperial';
-  const { Temperature } = data;
+  const { Temperature, WeatherIcon } = data;
   const { Value } = Temperature[system];
 
   const classes = useStyles();
   const symbols = { Metric: '℃', Imperial: '°F' };
-
+  console.log(WeatherIcon);
   return (
     <CardHeader
       avatar={<Avatar alt={city.name} src="" className={classes.large}>
-        <Icon color="inherit" fontSize="large">wb_sunny</Icon></Avatar>}
+        <Icon color="inherit" fontSize="large">{icons[WeatherIcon]}</Icon></Avatar>}
       action={<FavoriteButton city={city} />}
       title={<CityTitle title={city} />}
       subheader={<Typography variant="h4" color="textSecondary">{Value} {symbols[system]}</Typography>}
